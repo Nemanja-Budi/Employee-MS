5a9e605e8f8b11ca5631755b0f3111df1daac1b8
Nemanja Budimirovic <96budi@gmail.com>
2024-08-09 10:47:45 +0200

    Ispravak na paganation komponenti za abdejt trenutnog subjekta

--- src/app/shared/components/paganation/paganation.component.ts
+++ src/app/shared/components/paganation/paganation.component.ts
@@ -12,40 +12,39 @@ export class PaganationComponent {
   @Input({required: true}) currentSize!: BehaviorSubject<number>;
   @Input({required: true}) isNula!: BehaviorSubject<boolean>;
 
-
   visiblePages(): number[] {
     const maxPagesToShow = 3;
-    const startPage = Math.max(1, this.queryParamsSubject.value.pageNumber - 1);
+    const startPage = Math.max(1, this.queryParamsSubject.value.commonFilter.pageNumber - 1);
     const endPage = Math.min(this.currentSize.value, startPage + maxPagesToShow - 1);
     return Array.from({ length: endPage - startPage + 1 }, (_, i) => i + startPage);
   }
   
   goToPage(page: number): void {
-    this.queryParamsSubject.value.pageNumber = page;
-    this.updatePage(this.queryParamsSubject.value.pageNumber);
+    this.queryParamsSubject.value.commonFilter.pageNumber = page;
+    this.updatePage(this.queryParamsSubject.value.commonFilter.pageNumber);
   }
 
   goToFirstPage(): void {
-    this.queryParamsSubject.value.pageNumber = 1;
-    this.updatePage(this.queryParamsSubject.value.pageNumber);
+    this.queryParamsSubject.value.commonFilter.pageNumber = 1;
+    this.updatePage(this.queryParamsSubject.value.commonFilter.pageNumber);
   }
 
   goToLastPage(): void {
-    this.queryParamsSubject.value.pageNumber = this.currentSize.value
-    this.updatePage(this.queryParamsSubject.value.pageNumber);
+    this.queryParamsSubject.value.commonFilter.pageNumber = this.currentSize.value
+    this.updatePage(this.queryParamsSubject.value.commonFilter.pageNumber);
   }
 
   goToPreviousPage(): void {
-    if (this.queryParamsSubject.value.pageNumber > 1) {
-      this.queryParamsSubject.value.pageNumber--;
-      this.updatePage(this.queryParamsSubject.value.pageNumber);
+    if (this.queryParamsSubject.value.commonFilter.pageNumber > 1) {
+      this.queryParamsSubject.value.commonFilter.pageNumber--;
+      this.updatePage(this.queryParamsSubject.value.commonFilter.pageNumber);
     }
   }
 
   goToNextPage(): void {
-    if (this.queryParamsSubject.value.pageNumber < this.currentSize.value) {
-      this.queryParamsSubject.value.pageNumber++;
-      this.updatePage(this.queryParamsSubject.value.pageNumber);
+    if (this.queryParamsSubject.value.commonFilter.pageNumber < this.currentSize.value) {
+      this.queryParamsSubject.value.commonFilter.pageNumber++;
+      this.updatePage(this.queryParamsSubject.value.commonFilter.pageNumber);
     }
   }
 
