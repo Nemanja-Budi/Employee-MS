133f2105eea3395a79e462ca879be0348dbe62d2
Nemanja Budimirovic <96budi@gmail.com>
2024-09-25 10:15:01 +0200

    Dodata forma za unosenje datuma za kreiranje izvestaja

--- src/app/mp/izvestaji/izvestaji.component.ts
+++ src/app/mp/izvestaji/izvestaji.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GrupniIzvestaj } from 'src/app/models/mp/izvestaji/grupni.izvestaj.model';
+import { PojedinacniIzvestaj } from 'src/app/models/mp/izvestaji/pojedinacni.izvestaj.model';
+import { IzvestajiService } from './izvestaji.service';
 
 @Component({
   selector: 'app-izvestaji',
@@ -7,4 +12,58 @@ import { Component } from '@angular/core';
 })
 export class IzvestajiComponent {
 
+  izvestajiService: IzvestajiService = inject(IzvestajiService);
+
+  router: Router = inject(Router);
+  
+  // pojedinacniIzvestaj!: PojedinacniIzvestaj;
+  // grupniIzvestaj!: GrupniIzvestaj;
+  datumIzvestajiForm: FormGroup;
+  
+  @ViewChild('preview', { static: true }) preview!: ElementRef<HTMLDialogElement>;  
+
+  constructor(private fb: FormBuilder) {
+    this.datumIzvestajiForm = this.fb.group({
+      skipSifra: [false],
+      sifra: ['', [Validators.pattern('^[0-9]+$')]],
+      startDate: [''],
+      endDate: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (!this.datumIzvestajiForm.valid) return;
+  
+    const skipSifra = this.datumIzvestajiForm.value['skipSifra']; // Provera da li je checkbox označen
+    let sifra = this.datumIzvestajiForm.value['sifra'];
+    let startDate = this.datumIzvestajiForm.value['startDate'];
+    const endDate = this.datumIzvestajiForm.value['endDate'];
+  
+    if (startDate == "") {
+      startDate = '2024-01-01';
+    }
+  
+    if (skipSifra) {
+      this.izvestajiService.getIzvestaji(startDate, endDate).subscribe({
+        next: (response) => {
+          // this.grupniIzvestaj = response;
+          this.izvestajiService.grupniIzvestaj.next(response);
+          this.izvestajiService.endDate.next(endDate);
+          this.router.navigate(['/mp/izvestaji/izvestaj']);
+        },
+        error: (e) => console.error(e)
+      });
+    } else {
+      this.izvestajiService.getIzvestaj(sifra, startDate, endDate).subscribe({
+        next: (response) => {
+          // this.pojedinacniIzvestaj = response;
+          this.izvestajiService.pojedinacniIzvestaj.next(response);
+          this.izvestajiService.endDate.next(endDate);
+          this.router.navigate(['/mp/izvestaji/izvestaj']);
+        },
+        error: (e) => console.error(e)
+      });
+    }
+  }
+  
 }
--- src/app/mp/izvestaji/izvestaji.module.ts
+++ src/app/mp/izvestaji/izvestaji.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IzvestajiComponent } from './izvestaji.component';
 import { IzvestajiRoutingModule } from './izvestaji-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -11,7 +12,8 @@ import { IzvestajiRoutingModule } from './izvestaji-routing.module';
   ],
   imports: [
     CommonModule,
-    IzvestajiRoutingModule
+    IzvestajiRoutingModule,
+    SharedModule
   ]
 })
 export class IzvestajiModule { }
